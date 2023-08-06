07929295664233035d26cf42b6841a1bb0b450a2
Juan Antonio Cornejo Scappini <jacorsca@gmail.com>
2023-08-06 16:56:43 +0200

    Log: Add C++20's formatting library support

--- include/pistacchio/log.hh
+++ include/pistacchio/log.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <format>
 #include <string>
 #include <unordered_map>
 
@@ -34,4 +35,32 @@ public:
 	static void filter(const std::string& id, uint32_t level);
 private:
 	static std::unordered_map<std::string, uint32_t> s_filters;
+
+//
+// Template functions
+//
+public:
+	template<class... Args>
+	void info(std::format_string<Args...> fmt, Args&&... args)
+	{
+		info(std::vformat(fmt.get(), std::make_format_args(args...)));
+	}
+
+	template<class... Args>
+	void debug(std::format_string<Args...> fmt, Args&&... args)
+	{
+		debug(std::vformat(fmt.get(), std::make_format_args(args...)));
+	}
+
+	template<class... Args>
+	void warn(std::format_string<Args...> fmt, Args&&... args)
+	{
+		warn(std::vformat(fmt.get(), std::make_format_args(args...)));
+	}
+
+	template<class... Args>
+	void error(std::format_string<Args...> fmt, Args&&... args)
+	{
+		error(std::vformat(fmt.get(), std::make_format_args(args...)));
+	}
 };
